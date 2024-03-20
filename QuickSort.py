cfa9c1c9583c30fbc1dd1a5c4add063bbf1a9a90
ejders-bzz <ejders@bzz.ch>
2024-03-20 15:17:43 +0100

    quicksort works

--- QuickSort.py
+++ QuickSort.py
@@ -1,5 +1,43 @@
+import time
+import csv
+
+
+def quick_sort(list, param):
+    left = []
+    right = []
+    equal = []
+
+    if len(list) > 1:
+        pivot = list[-1][param]
+        print(pivot)
+        for i in list:
+            if i[param] < pivot:
+                left.append(i)
+            elif i[param] == pivot:
+                equal.append(i)
+            elif i[param] > pivot:
+                right.append(i)
+
+        return quick_sort(left, param) + equal + quick_sort(right, param)
+    else:
+        return list
+
+
 def main():
-    print('Deez Nuts!')
+    unsorted_list = []
+    file = open('data/SortSmall.txt')
+    data = csv.reader(file, delimiter=',')
+    for row in data:
+        print(row)
+        unsorted_list.append(row)
+    print(unsorted_list)
+
+    start = time.perf_counter()
+
+    print(quick_sort(unsorted_list, 2))
+
+    end = time.perf_counter()
+    print(f'Time: {end - start}')
 
 
 if __name__ == '__main__':
